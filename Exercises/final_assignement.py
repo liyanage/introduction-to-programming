adc10ca29fd3f4e6255ee0ae423999e0181bbda7
Vera Carr <veracarr@apple.com>
2015-07-10 23:23:26 -0700

    dictionary and functions programming exercises

---
+++ Exercises/final_assignement.py
@@ -0,0 +1,11 @@
+kelly    = {'weight':146, 'height':70, 'age':42, 'gender':'female'}
+barack   = {'weight':180, 'height':73, 'age':53, 'gender':'male'}
+michelle = {'weight':160, 'height':71, 'age':51, 'gender':'female'}
+buddy    = {'weight':43,  'height':40, 'age':4,  'gender':'male'}
+
+people = {'kelly':kelly, 'barack':barack, 'michelle':michelle, 'buddy':buddy}
+
+# Problem:
+# Ask name for person who's data user would like to retrieve
+# Ask what data the user would like for that person (possible options:'BMI', 'gender', 'year of birth')
+# Compute the data where needed an return the resulting data
